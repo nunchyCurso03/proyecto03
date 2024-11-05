55040cdde02b397268931a573a82e2c4a6d1fa41
curso03 <tuemail@example.com>
2024-11-05 19:50:49 +0100

    incluyo el paquete proyecto03

---
+++ src/main/java/es/santander/ascender/proyecto03/App.java
@@ -0,0 +1,13 @@
+package es.santander.ascender.proyecto03;
+
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        long resultado = calculadora.sumar(4, 5);
+        System.out.println(resultado);
+    }
+}
