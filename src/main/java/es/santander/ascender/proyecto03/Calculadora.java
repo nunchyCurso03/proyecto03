f96182c292ac0a12073dbd5c8d6d232ade0a47c7
curso03 <you@example.com>
2024-11-06 11:19:36 +0100

    test funcionando

--- src/main/java/es/santander/ascender/proyecto03/Calculadora.java
+++ src/main/java/es/santander/ascender/proyecto03/Calculadora.java
@@ -1,19 +1,26 @@
 package es.santander.ascender.proyecto03;
 
 public class Calculadora {
-    public long sumar(long sumando1, long sumando2) {
+
+    public long sumar (long sumando1, long sumando2){
         return sumando1 + sumando2;
-    }
 
-    public long restar(long substraendo, long minuendo) {
+    }
+    
+    public long restar (long substraendo, long minuendo){
         return substraendo - minuendo;
+
     }
 
-    public long multiplicar(long factor1, long factor2) {
+    public long multiplicar (long factor1, long factor2){
         return factor1 * factor2;
     }
 
-    public double dividir(long dividendo, long divisor) {
+    public double dividir (long dividendo, long divisor){
         return dividendo / divisor;
+
     }
+
+
 }
+ 
--- src/test/java/es/santander/ascender/proyecto03/CalculadoraTest.java
+++ src/test/java/es/santander/ascender/proyecto03/CalculadoraTest.java
@@ -6,38 +6,41 @@ import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
 
-    //PREGUNTAR¿crear una new Calculadora en cada método o una fuera común?-->BORRAR
-    //TODO ¿donde creo el método para dividir por cero? (NO ESTÁ CREADO)
-      @Test
+    // PREGUNTAR¿crear una new Calculadora en cada método o una fuera
+    // común?-->BORRAR
+    // TODO ¿donde creo el método para dividir por cero? (NO ESTÁ CREADO)
+    @Test
     public void testSumar() {
         // Preparo
         Calculadora calculadora = new Calculadora();
-        
+
         // Ejecuto
         long resultadoSumar = calculadora.sumar(25, 5);
-        
+
         // Verifico
         assertEquals(30, resultadoSumar);
     }
+
     @Test
     public void testRestar() {
         // Preparo
         Calculadora calculadora = new Calculadora();
-        
+
         // Ejecuto
         long resultadoRestar = calculadora.restar(12, 2);
-        
+
         // Verifico
         assertEquals(10, resultadoRestar);
     }
+
     @Test
     public void testMultiplicar() {
         // Preparo
         Calculadora calculadora = new Calculadora();
-        
+
         // Ejecuto
         long resultadoMultiplicar = calculadora.multiplicar(20, 3);
-        
+
         // Verifico
         assertEquals(60, resultadoMultiplicar);
     }
@@ -46,11 +49,11 @@ public class CalculadoraTest {
     public void testDividir() {
         // Preparo
         Calculadora calculadora = new Calculadora();
-        
+
         // Ejecuto
-        double resultado = calculadora.dividir(50, 2);
-        
+        double resultadoDividir = calculadora.dividir(50, 2);
+
         // Verifico
-        assertEquals(25.0, resultado);
+        assertEquals(25.0, resultadoDividir);
     }
 }
